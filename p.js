0cc7393fce1a3a5f8369c082d691d1aec9ad189a
israel ashenafi <israelashenafi@gmail.com>
2025-05-04 12:02:40 -0700

    added phone menu and finished it

--- p.js
+++ p.js
@@ -1,4 +1,5 @@
 const links = document.querySelectorAll(`.navbar_links`)
+const sections = document.querySelectorAll(`section`)
 
 links.forEach(link => {
     link.addEventListener(`click`, function(){
@@ -8,3 +9,24 @@ links.forEach(link => {
         link.classList.add(`active`)
     })
 });
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry =>{
+            if(entry.isIntersecting){
+                links.forEach(link =>{
+                    link.classList.remove("active");
+                    if (link.getAttribute("href").substring(1) === entry.target.id) {
+                        link.classList.add("active");
+                      }
+                })
+            }
+        })
+    },
+    {
+        threshold :0.6,
+    }
+)
+sections.forEach(section => {
+    observer.observe(section);
+})
